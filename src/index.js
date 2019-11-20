9e1107c3c83ad505c26faae38cd15e92f9fecc2c
Sólveig Ásta Friðriksdóttire <solveigasfr@gmail.com>
2019-11-20 21:18:52 +0000

    javascript updated / rollup and babel installed

--- src/index.js
+++ src/index.js
@@ -1,13 +1,27 @@
-import List from './lib/list';
+
+import { createListItem } from './lib/helpers';
+
+// Fetches lectures.json asynchronously from root of project
+async function getLectures() {
+  const response = await fetch('lectures.json');
+  const data = await response.json();
+
+  return data.lectures;
+}
+
+async function init() {
+  // Get lectures as an array
+  const lectures = await getLectures();
+
+  // Loop through all the lectures
+  for (let i = 0; i < lectures.length; i += 1) {
+    const lecture = lectures[i];
+    // Create html chunk for each lecture
+    const listItemHtml = createListItem(lecture);
+  }
+}
+
 
 document.addEventListener('DOMContentLoaded', () => {
-  const page = document.querySelector('body');
-  const isLecturePage = page.classList.contains('lecture-page');
-
-  if (isLecturePage) {
-
-  } else {
-    const list = new List();
-    list.load();
-   }
+  init();
 });
--- src/lib/helpers.js
+++ src/lib/helpers.js
@@ -1,5 +1,9 @@
-export function empty(element) {
+export default function empty(element) {
   while (element.firstChild) {
     element.removeChild(element.firstChild);
   }
+}
+
+export function createListItem(lecture) {
+  console.log('creating list item lecture', lecture);
 }
